84c5f1831c24d136eb69afad8d1a77abb3c1b37a
Arttu Siren <arttu.siren@gmail.com>
2017-12-16 17:24:35 +0200

    disable web interface

--- index.js
+++ index.js
@@ -13,9 +13,10 @@ const gameserver = require('./src/gameserver');
 app.use(express.static(path.join(__dirname, 'webapp')));
 gameserver.init(io);
 
+/*
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'webapp', 'index.html'));
-});
+}); */
 
 http.listen(serverconfig.webserver_port, serverconfig.webserver_bind, () => {
   console.log(`[WEBSERVER]: Listening on ${serverconfig.webserver_bind}:${serverconfig.webserver_port}`);
