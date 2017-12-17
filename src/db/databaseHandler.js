e5d013fb223384f019d243e3a8a63feebe67ff14
Arttu Siren <arttu.siren@gmail.com>
2017-12-17 18:03:17 +0200

    add connection check to dbhandler

--- src/db/databaseHandler.js
+++ src/db/databaseHandler.js
@@ -11,15 +11,33 @@ const pgconfig = {
   idleTimeoutMills: 30000,
 };
 
-const pool = new pg.Pool(pgconfig);
+const debugpgconfig = {
+  user: 'spacteria',
+  database: 'spacteriagame',
+  password: 'postgres',
+  port: 5432,
+  max: 10,
+  idleTimeoutMills: 30000,
+};
 
-pool.on('error', (err, client) => {
-  console.log('idle client error', err.message, err.stack, client);
-});
+let pool;
+
+function configureDBConnection() {
+  pool = new pg.Pool(pgconfig);
+
+  pool.on('error', (err, client) => {
+    console.log('idle client error', err.message, err.stack, client);
+  });
+}
+
+configureDBConnection();
 
 module.exports = {
   getConnection() {
     return new Promise((resolve) => {
+      if (pool === undefined || pool === null) {
+        configureDBConnection();
+      }
       pool.connect((err, client, done) => {
         if (err) {
           console.log(err);
